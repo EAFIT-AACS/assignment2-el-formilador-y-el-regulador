bbc3720f1a354acf2d525f8ea55e7179e0021308
Jeronimo Campuzano Castaño <jcampuzanc@eafit.edu.co>
2025-03-19 21:41:39 -0500

    main updated to execute Algorithm2

--- ALGORITHM_2_LFCO_2025_JC_AP.cpp
+++ ALGORITHM_2_LFCO_2025_JC_AP.cpp
@@ -1,9 +1,37 @@
 #include "ALGORITHM_2_LFCO_2025_JC_AP.h"
+#include <stack>
 
-bool ALGORITHM_2_LFCO_2025_JC_AP::stringCheck(string string) {
-    return false;
+bool ALGORITHM_2_LFCO_2025_JC_AP::stringCheck(string string_to_Check) {
+        stack<char> stack;
+
+        //If the string's length is 0, the PDA will accept it
+        if (string_to_Check.empty()) {
+            return true;
+        }
+        
+        //If the string is odd, the PDA will reject it
+        if (string_to_Check.length() % 2 == 1) {
+            return false;
+        }
+        
+        //If the string's length is even, we check if the string is accepted by the PDA
+        for (int j = 0; j < string_to_Check.length(); j++) {
+            if (string_to_Check[j] == 'a') {
+                stack.push('A');
+            } else {
+                if (string_to_Check[j] == 'b') {
+                    if(!stack.empty()) {
+                        stack.pop();
+                    }
+                    
+                }
+            }
+        }
+
+        if (stack.empty()) {
+            return true;
+        } else {
+            return false;
+        }
 }
 
-/*
-
-*/
--- main.cpp
+++ main.cpp
@@ -8,21 +8,26 @@ using namespace std;
 
 int main() {
 
-    ALGORITHM_1_LFCO_2025_JC_AP p;
+    ALGORITHM_1_LFCO_2025_JC_AP p1;
 
     //Generate 4 strings and put them into a vector, 2 valid and 2 invalid
     vector<string> strings;
     for (int i = 0; i < 4; i++)
     {
         if (i % 2 == 0) {
-            strings.push_back(p.generateGrammarStrings());
+            strings.push_back(p1.generateGrammarStrings());
         } else {
-            strings.push_back(p.generateNonGrammarStrings());
+            strings.push_back(p1.generateNonGrammarStrings());
         }
     }
     
     for (int i = 0; i < strings.size(); i++) {
         cout << strings[i] << endl;
+    }
+    
+    ALGORITHM_2_LFCO_2025_JC_AP p2;
+    for (int i = 0; i < strings.size(); i++) {
+        cout << p2.stringCheck(strings[i]) << endl;
     }
 
 
